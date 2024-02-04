88eda8d8b210e65d6fc8bf0a6709b87cafd0541b
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-02-04 10:20:16 +0530

    uodate css for stories details

--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -73,29 +73,6 @@ const sidebarElements = [
 ];
 
 const Sidebar = () => {
-  const [visible, setVisible] = useState(true);
-  const navDataSmall = () => {
-    return (
-      <div className="openNavBar">
-        <img
-          className="img-navbar"
-          src="https://res.cloudinary.com/drvjsegeb/image/upload/v1706711272/cancel-close_nkzevb.svg"
-          alt="Close"
-          onClick={() => setVisible(!visible)}
-        />
-        <ul className="">
-          {sidebarElements.map((element) => (
-            <SidebarElement key={element.id} data={element} />
-          ))}
-        </ul>
-        <div className="contact-support-cont">
-          <MdOutlineHeadset className="contact-support-icon" />
-          <p className="contact-support-text">Contact Support</p>
-        </div>
-      </div>
-    );
-  };
-
   return (
     <div className="sidebar-bg-container">
       <ul className="sidebar-inner-container-big-screen">
@@ -108,19 +85,6 @@ const Sidebar = () => {
           <p className="contact-support-text">Contact Support</p>
         </div>
       </ul>
-
-      <div className="sidebar-inner-container-small-screen">
-        {visible ? (
-          <div className="closedNavbar-cont">
-            <GiHamburgerMenu
-              onClick={() => setVisible(!visible)}
-              className="closedNavbar"
-            />
-          </div>
-        ) : (
-          navDataSmall()
-        )}
-      </div>
     </div>
   );
 };
